032ae48eeaca9d5aa79112a627fef921fae54bb3
Carlos Cabrera <carloscabreraq@gmail.com>
2023-02-22 17:13:02 -0400

    se agregó entidad permisos

--- src/modules/permissions/entities/permission.entity.ts
+++ src/modules/permissions/entities/permission.entity.ts
@@ -1,1 +1,32 @@
-export class Permission {}
+import { Entity, PrimaryGeneratedColumn, Column, Index } from 'typeorm';
+@Entity({ schema: 'auth', name: 'permissions' })
+export class PermissionEntity{
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Index()
+  @Column({ type: 'varchar', length: 60, nullable: false, unique: true })
+  public slug: string;
+
+  @Column({ type: 'varchar', length: 20, nullable: true })
+  public resource: string;
+
+  @Column({ type: 'varchar', length: 20, nullable: true })
+  public action: string;
+
+  @Column({
+    name: 'description',
+    type: 'varchar',
+    nullable: false,
+    length: 160,
+  })
+  description: string;
+
+  @Column({
+    name: 'active',
+    type: 'boolean',
+    nullable: false,
+    default: true,
+  })
+  active: boolean;
+}

d2af3d7ca074cec38d8dd3cfe8ca82f422e2982a
Carlos Cabrera <carloscabreraq@gmail.com>
2023-05-16 19:31:11 -0400

    Configuracion de la APP

--- src/config/auth.config.ts
+++ src/config/auth.config.ts
@@ -2,6 +2,6 @@ import { registerAs } from '@nestjs/config';
 
 export default registerAs('auth', () => ({
   jwtAccessSecret: process.env.JWT_ACCESS_SECRET,
-  tokenExpires: parseInt(process.env.JWT_TOKEN_EXPIRES_IN, 10),
-  refreshTokenExpires: parseInt(process.env.JWT_REFRESHTOKEN_EXPIRES_IN, 10),
+  tokenExpires: parseInt(process.env.JWT_TOKEN_EXPIRES_IN, 10) || 900,
+  refreshTokenExpires: parseInt(process.env.JWT_REFRESHTOKEN_EXPIRES_IN, 10) || 604800,
 }));
